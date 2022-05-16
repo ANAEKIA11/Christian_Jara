553502ff1eefe3bdad392ae0108f30e6cca5c1d7
Christian Jara <cristianjara193@gmail.com>
2022-05-16 18:53:01 -0500

    Final

---
+++ app/src/main/java/com/example/jara_abad/MainActivity.kt
@@ -0,0 +1,21 @@
+package com.example.jara_abad
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+public class MainActivity : AppCompatActivity() {
+    //override fun onCreate(savedInstanceState: Bundle?) {
+      //  super.onCreate(savedInstanceState)
+       // setContentView(R.layout.activity_main)
+
+    //}
+    private Button siguiente;
+    private Button MostrarResultado;
+    private textviewnombre;
+    private textviewApellido;
+    private textviewBase;
+
+}
+public void (){
+ string nombre = Christian;
+}
---
+++ app/src/main/java/com/example/jara_abad/MainActivity2.kt
@@ -0,0 +1,14 @@
+package com.example.jara_abad
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity2 : AppCompatActivity() {
+    private textview nombre;
+    private textview base;
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main2)
+    }
+}
